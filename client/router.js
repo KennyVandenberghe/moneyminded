a25327ab880886d6ae5a718a9e2c58cbacf6795d
Kenny Vandenberghe <kenny.vandenberghe@commercieelwebverkeer.be>
2015-06-22 22:38:54 +0200

    updates

--- client/router.js
+++ client/router.js
@@ -1,15 +1,14 @@
 Router.configure({
-	layoutTemplate: 'layout'
+  layoutTemplate: 'layout'
 });
 
 Router.route('/', {
-	name: 'accounts'
+  name: 'accounts'
 });
 
 Router.route('/account/:_id', {
-	name: 'account',
-	data: function() {
-		return MM.Accounts.findOne(this.params._id);
-	}
+  name: 'account',
+  data: function() {
+    return MM.Accounts.findOne(this.params._id);
+  }
 });
-
